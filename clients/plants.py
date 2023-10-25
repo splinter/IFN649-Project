05bad97cf167988f6ebfeb08a3787f3dc83f0988
splinter <mgjaye@gmail.com>
2023-10-26 02:24:12 +1000

    Send data as json

--- clients/plants.py
+++ clients/plants.py
@@ -7,9 +7,11 @@ configs.read("clients.ini")
 
 def update_plant_data(deviceID,plantID, humidity):
     endpoint = configs["default"]["endpoint"] + "/plants/sensors?deviceID=1"
-    results = requests.put(endpoint,data= {
+    results = requests.put(endpoint,json={
         "deviceID": deviceID,
         "plantID": plantID,
         "humidity": humidity
+    }, headers= {
+        "Content-Type": "application/json"
     })
     return
